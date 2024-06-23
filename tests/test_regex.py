8b5462bbbd9b6379a654df0fa5e7729c2e16bc3a
Roman Bulakh <bulah.roman@gmail.com>
2024-06-23 13:05:45 +0200

    add static routes for test_regex

--- tests/test_regex.py
+++ tests/test_regex.py
@@ -4,8 +4,9 @@ route_lines = [
     "                   unreachable [HIVANE 2017-01-11 from 193.17.192.135] (100/-) [AS47583i]",
     "                   via 206.41.110.21 on bond0.895 [transit_as53264_nchc 2016-11-22] (100) [AS29713i]",
     "10.255.30.0/24     blackhole [static1 2017-01-14] * (200)",
-]
-
+    "2001:db8:123::/64  dev eth0 [direct1 10:14:07] * (240)",
+    "                   via 2001:db8:123::10 on eth0 [rs1_ipv6 10:14:10] (100) [AS65010i]",
+ ]
 
 def test_route_summary(bird):
     for each in route_lines:
